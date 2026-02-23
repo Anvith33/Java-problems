20de9d87add954c594234ba77b34059477d2ac63
Anvith C L <anvithchakote33@gmail.com>
2026-02-23 14:55:26 +0530

    Add files via upload
    A Java program that finds the fibonacci series up to a user specified.

---
+++ fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number of fibonacci series:");
+        int n=scanner.nextInt(); 
+        int a=0, b=1;
+        System.out.println("Fibonacci Series:");
+        for(int i=1; i<=n; i++){
+            System.out.print(a + " ");
+            int next=a+b;
+            a=b;
+            b=next;
+        }
+    }
+
+    
+}
