b8749b9c02ea9c8a31707d2da57362bf68a73d0e
Anvith C L <anvithchakote33@gmail.com>
2026-03-12 00:38:55 +0530

    Refactor Fibonacci series output logic

--- fibonacci.java
+++ fibonacci.java
@@ -3,16 +3,19 @@ public class fibonacci {
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the number of fibonacci series:");
-        int n=scanner.nextInt(); 
-        int a=0, b=1;
-        System.out.println("Fibonacci Series:");
-        for(int i=1; i<=n; i++){
-            System.out.print(a + " ");
-            int next=a+b;
+        int n=scanner.nextInt();
+        int a=0,b=1;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=0;i<=n-3;i++)
+        {
+            int c=a+b;
+            System.out.println(+c);
             a=b;
-            b=next;
+            b=c;
         }
     }
-
     
 }
+
+
